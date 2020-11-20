c68438e4c5c044ad252680a3355b9738d78116e4
Axel S <saxelsyeguillaume@gmail.com>
2020-11-20 21:21:50 +0100

    Added config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,59 @@
 (() => {
+    function createGridConfig() {
+        let sizes = document.createElement('div');
+
+        let x = document.createElement('INPUT');
+        x.setAttribute('id', 'x');
+        x.setAttribute('type', 'number');
+
+        let y = document.createElement('INPUT');
+        y.setAttribute('id', 'y');
+        y.setAttribute('type', 'number');
+
+        sizes.appendChild(x);
+        sizes.appendChild(y);
+
+        return sizes;
+    }
+
+    function createSubmitButton() {
+        let b = document.createElement('button');
+        b.appendChild(document.createTextNode('GO'));
+        b.addEventListener('click', () => {
+            console.log('SUBMIT');
+        });
+        return b;
+    }
+
+    function createDifficulties() {
+        let select = document.createElement('SELECT');
+        select.setAttribute('id', 'difficulty');
+
+        let eOpt = document.createElement('option');
+        eOpt.appendChild(document.createTextNode('Easy'));
+        eOpt.value = '25';
+        select.appendChild(eOpt);
+
+        let mOpt = document.createElement('option');
+        mOpt.appendChild(document.createTextNode('Medium'));
+        select.value = '50';
+        select.appendChild(mOpt);
+
+        let hOpt = document.createElement('option');
+        hOpt.appendChild(document.createTextNode('Hard'));
+        hOpt.value = '75';
+        select.appendChild(hOpt);
+        return select;
+    }
+    function createConfig() {
+        let config = document.createElement('div');
+        config.setAttribute('id', 'config');
+
+        config.appendChild(createDifficulties());
+        config.appendChild(createGridConfig());
+        config.appendChild(createSubmitButton());
+        return config;
+    }
     const createColumn = (row) => {
         let button = document.createElement('button');
         button.appendChild(document.createTextNode('⬛'));
@@ -10,6 +65,7 @@
     const createRow = () => document.createElement('tr');
 
     const lose = () => {
+        alert('YOU LOOSE');
         //TODO on click bomb ded && afficher toutes bombes
     };
 
@@ -17,10 +73,14 @@
         //TODO if toutes td dont td.id !== bomb clickées alors win
     };
 
-    function revealEmpty() {}
+    function revealEmpty(col) {
+        console.log(col);
+        col.style.backgroudColor = 'green';
+    }
 
     const createGrid = (x, y, bombNumber) => {
         let bombs = bombNumber;
+        let emptyBoxes = x * y - bombNumber;
         let putBomb;
         let table = document.createElement('table');
         for (let j = 0; j < y; j++) {
@@ -28,19 +88,19 @@
             table.appendChild(row);
             for (let i = 0; i < x; i++) {
                 putBomb = Math.floor(Math.random() * 100) <= 25 && bombs > 0;
-                if (putBomb) bombs--;
 
-                let col = createColumn(row, putBomb);
+                let col = createColumn(row);
                 if (putBomb) {
+                    bombs--;
                     col.setAttribute('id', 'bomb');
-                    col.addEventListener('click', () => {
-                        //TODO ajouter detecteur de click si bombe
-                        alert('YOU LOOSE');
-                    });
+                    col.addEventListener(
+                        'click',
+                        () => lose() //TODO ajouter detecteur de click si bombe
+                    );
                 } else {
                     col.setAttribute('id', 'vide');
                     col.addEventListener('click', () => {
-                        //TODO ajouter detecteur de click si vide
+                        revealEmpty(col);
                     });
                 }
             }
@@ -50,5 +110,6 @@
 
     let app = document.getElementById('grid');
 
+    app.appendChild(createConfig());
     app.appendChild(createGrid(10, 10, 20));
 })();
